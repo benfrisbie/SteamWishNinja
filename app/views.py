b6dbf0292146e2cefebc4e05bcea717ab720a1a1
Ben Frisbie <bbfrisbi@mtu.edu>
2014-06-26 20:56:18 -0400

    removed some things to clear confusion

--- app/views.py
+++ app/views.py
@@ -3,11 +3,8 @@
 # Holds all the handlers that will respond to requests from the browser.
 # Each python function in here will map to 1 or more request URLs.
 # *************************************************************************
-from flask import render_template, redirect, url_for, g
+from flask import render_template, redirect, url_for
 from app import app, oid
-import re
-
-_steam_id_re = re.compile('steamcommunity.com/openid/id/(.*?)$')
 
 # The first page/home that will be displayed first
 # routed to both the views
@@ -17,12 +14,13 @@ def index():
     return render_template('index.html', title = 'Home')
 
 
-# log in a user using Steam OpenID
+# Log in a user using Steam OpenID
 @app.route('/login')
 @oid.loginhandler
 def login():
     return oid.try_login('http://steamcommunity.com/openid')
 
+# Called after a login
 @oid.after_login
 def create_or_login(resp):
     return 'You signed in!'
