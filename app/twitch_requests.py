afbf89fb07978604f017803d8d3d6e6ec3d0f131
frankcash <cfcash@mtu.edu>
2014-06-28 08:11:36 -0400

    Fixes TwitchTV route.  It wasn't actually running.

--- app/twitch_requests.py
+++ app/twitch_requests.py
@@ -4,8 +4,11 @@
 # *******************************************************************************
 
 import requests, json
+from flask import jsonify
+from app import app, oid, db
 
 # Simply gets the top games on Twitch.TV
+@app.route('/topgames')
 def topgames():
     toplist=requests.get('https://api.twitch.tv/kraken/games/top')
     return jsonify(toplist.json())
