b02b1dca55ea24e7490046b951f78b9507f82b7e
Ben Frisbie <bbfrisbi@mtu.edu>
2014-07-12 12:44:03 -0400

    removed the copy of the method i was messing with

--- app/twitch_requests.py
+++ app/twitch_requests.py
@@ -10,19 +10,7 @@ from flask import jsonify
 def topgames():
     toplist = requests.get('https://api.twitch.tv/kraken/games/top')
     return jsonify(toplist.json())
-
-
-# Search twitch for a specific game
-def searchgame(game):
-    url = 'https://api.twitch.tv/kraken/search/streams/?q=%s' %game
-    gameSearch = requests.get(url).json()
-    if(gameSearch['_total']!=0):
-
-        channel=gameSearch['streams'][0]['channel']['name']
-    else:
-        channel=""
-    return channel
-
+    
 
 # Search twitch for a specific game
 def searchgame(game):
