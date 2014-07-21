d3705283c939bd22b98b730db984fa115357c5c5
Frank Cash <cfcash@mtu.edu>
2014-07-20 22:25:16 -0400

    Starts a Pcgamer requests set

---
+++ app/pcgamer_requests.py
@@ -0,0 +1,16 @@
+# ****************************** pcgamer_requests.py ****************************
+# *                     because fuck API's and fuck PCGamer                     *
+# *******************************************************************************
+
+import requests, json
+from flask import jsonify
+from bs4 import BeautifulSoup
+
+# baseUrl="http://www.pcgamer.com/search/%s"
+
+def searchPcGame(game):
+    #need to parse id="search_items"
+    url = 'http://www.pcgamer.com/search/%s' %game
+    rv = requests.get(url)
+    soup=BeautifulSoup(rv.text)
+    soup.find_all(id="search_items")
